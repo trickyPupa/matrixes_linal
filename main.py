b8b2191f463977f22ea58de387431682aa8987bd
trickyPupa <timofeylabor@gmail.com>
2024-01-13 14:37:43 +0300

    release

--- main.py
+++ main.py
@@ -4,36 +4,6 @@ from technical import *
 from func import eval_
 
 
-'''def create_exp(source, matrix_valuables, vector_valuables, brackets):
-    # exp = [] if brackets else [[]]
-    exp = [[]]
-
-    while True:
-        i = source.read(1)
-        if i == '\n':
-            return exp
-        if i in " *":
-            continue
-        if i == "x":
-            pass
-        elif i == "+":
-            exp.append([])
-        elif i in "([":
-            temp_summand = create_exp(source, matrix_valuables, vector_valuables, brackets + i)
-            exp[-1] += temp_summand
-        elif i in ")]":
-            if brackets[-1] != brackets_dict[i]:
-                raise Exception("Ошибка в записи выражения")
-            print(exp)
-            return exp
-        elif i.isupper():
-            exp[-1].append(i)
-            matrix_valuables[i] = Matrix()
-        elif i.islower():
-            exp[-1].append(i)
-            vector_valuables[i] = Vector()'''
-
-
 def solution(filename="", file=False):
     if file:
         with open(filename) as f:
@@ -62,6 +32,12 @@ def solution(filename="", file=False):
         elif a[i] in "()+*":
             expression += a[i]
     expression += a[-1]
+    if a[-1].isupper() and a[-1] not in queue[0]:
+        queue[0] += a[-1]
+        valuables[a[-1]] = Matrix()
+    elif a[-1] != 'x' and a[-1].islower() and a[-1] not in queue[1]:
+        queue[1] += a[-1]
+        valuables[a[-1]] = valuables.get(a[-1], Vector())
 
     # запись значений матриц и векторов
     it = iter(data[1:])
@@ -76,97 +52,26 @@ def solution(filename="", file=False):
         mat = []
         for _ in range(rows):
             mat.append(list(map(int, it.__next__().split())))
-
-        print(rows, cols, mat)
         assert len(mat) == rows and len(mat[0]) == cols
         valuables[k].set_data(mat)
     for k in queue[1]:
         n = int(it.__next__())
         vec = [list(map(int, it.__next__().split()))]
         valuables[k].set_data(vec)
-
-        print(n, vec)
         assert len(vec[0]) == n
 
-    # print(expression)
-    # print(valuables)
-
-
-'''    ans = ""
-    matrix_valuables = OrderedDict()
-    vector_valuables = OrderedDict()
-    expression = create_exp(f, matrix_valuables, vector_valuables, '')
-
-    print(matrix_valuables)
-    print(vector_valuables)
+    # print(queue)
     print(expression)
+    print(valuables)
 
-    # for i in f.readline().replace(" ", "").strip():
-    #     if i == "x":
-    #         pass
-    #     elif i == "+":
-    #         expression.append([])
-    #         cur_address[-1] += 1
-    #     elif i in "([<":
-    #         expression[-1].append([])
-    #     elif i == ")]>":
-    #         del cur_address[-1]
-    #     elif i.isupper():
-    #         expression[-1].append(i)
-    #         matrix_valuables[i] = Matrix()
-    #     elif i.islower():
-    #         expression[-1].append(i)
-    #         vector_valuables[i] = Vector()
-
-    # while True:
-    #     i = f.read(1)
-    #     if i == '\n':
-    #         break
-    #
-    #     if i in " *":
-    #         continue
-    #     if i == "x":
-    #         pass
-    #     elif i == "+":
-    #         expression.append([])
-    #     elif i in "([<":
-    #         temp_summand = create_summand(f)
-    #         expression[-1].append(temp_summand)
-    #     # elif i == ")]>":
-    #     #     del cur_address[-1]
-    #     elif i.isupper():
-    #         expression[-1].append(i)
-    #         matrix_valuables[i] = Matrix()
-    #     elif i.islower():
-    #         expression[-1].append(i)
-    #         vector_valuables[i] = Vector()
-
-    for i in matrix_valuables.keys():
-        rows_n_cols = list(map(int, f.readline().split()))
-        rows = rows_n_cols[0]
-        if len(rows_n_cols) == 1:
-            cols = rows
-        else:
-            cols = rows_n_cols[1]
-
-        mat = []
-        for _ in range(rows):
-            mat.append(list(map(int, f.readline().split())))
-        assert len(mat) == rows and len(mat[0]) == cols
-        matrix_valuables[i].set_data(mat)
-
-    for i in vector_valuables.keys():
-        n = int(f.readline())
-        vec = [list(map(int, f.readline().split()))]
-        vector_valuables[i].set_data(vec)
-        assert len(vec[0]) == n
-
-    pass
-
-    print(matrix_valuables)
-    print(vector_valuables)
-
-    print(ans)'''
+    res = eval_(expression, valuables)
+    print(res)
+    ans = res.solve()
+    if type(ans) == str:
+        print(ans)
+    else:
+        for i in range(len(ans)):
+            print(f"x{i + 1} = {ans[i]}")
 
 
 if __name__ == "__main__":
--- technical.py
+++ technical.py
@@ -1,16 +1,70 @@
+from itertools import combinations
+
+
+def minor(matrix, ai, aj):
+    rows, cols = len(matrix), len(matrix[0])
+    return [[matrix[i][j] for j in range(cols) if j != aj] for i in range(rows) if i != ai]
+
+
 def determinant(matrix):
     rows, cols = len(matrix), len(matrix[0])
-
-    def minor(ai, aj):
-        return [[matrix[i][j] for j in range(cols) if j != aj] for i in range(rows) if i != ai]
 
     if rows != cols:
         return None
     else:
+        if rows == 1:
+            return matrix[0][0]
         if rows == 2:
             return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
         else:
-            return sum((-1) ** j * matrix[0][j] * determinant(minor(0, j)) for j in range(cols))
+            return sum((-1) ** j * matrix[0][j] * determinant(minor(matrix, 0, j)) for j in range(cols))
+
+
+def gauss(a, y):
+    n = len(a[0])
+
+    eps = 0.000001
+    x = [0] * n
+    k = 0
+    while k < n:
+        # поиск строки с максимальным a[i][k]
+        mx = abs(a[k][k])
+        index = k
+        for i in range(k + 1, n):
+            if abs(a[i][k]) > mx:
+                mx = abs(a[i][k])
+                index = i
+
+        # перестановка строк
+        if mx < eps:
+            # нет ненулевых диагональных элементов
+            print(f"Нет решения, так как есть нулевой столбец {index}")
+            return None
+
+        a[k], a[index] = a[index], a[k]
+        y[k], y[index] = y[index], y[k]
+
+        # нормализация уравнений
+        for i in range(k, n):
+            temp = a[i][k]
+            if abs(temp) < eps:
+                continue
+            a[i] = [a[i][j] / temp for j in range(n)]
+            y[i] = y[i] / temp
+            if i == k:
+                continue
+
+            a[i] = [a[i][j] - a[k][j] for j in range(n)]
+            y[i] -= y[k]
+
+        k += 1
+
+    # обратная подстановка
+    for k in range(n - 1, -1, -1):
+        x[k] = y[k]
+        y = [y[i] - a[i][k] * x[k] for i in range(k)]
+
+    return x
 
 
 class Matrix:
@@ -37,30 +91,27 @@ class Matrix:
             self.cols = 0
             self.matrix = []
 
-        # print(self.matrix)
-        # print(self.n, self.m)
-
     def __add__(self, other):
         if isinstance(other, Matrix):
             if other.rows == self.rows and other.cols == self.cols:
                 result = [[self.matrix[i][j] + other.matrix[i][j] for j in range(self.cols)] for i in range(self.rows)]
                 return Matrix(data=result)
             else:
-                raise ValueError("Сложение матриц разных размеров")
+                raise ValueError(f"Сложение матриц разных размеров:\n{self}\t{other}")
+        elif isinstance(other, SLAU):
+            return other + self
         elif other is None:
             return self
         else:
-            raise TypeError("Неподходящее слагаемое для матрицы")
+            raise TypeError(f"Неподходящее слагаемое для матрицы {self}:" + other)
 
     def __mul__(self, other):
         if isinstance(other, Matrix):
             if self.cols != other.rows:
-                raise ValueError("Матрицы неподходящих размеров для перемножения")
+                raise ValueError(
+                    f"Матрицы неподходящих размеров для перемножения:\n{self.__repr__()}\t{other.__repr__()}")
             res = [[sum(self.matrix[i][k] * other.matrix[k][j] for k in range(self.cols)) for j in range(other.cols)]
                    for i in range(self.rows)]
-            # for i in range(self.rows):
-            #     for j in range(other.cols):
-            #         res[i][j] = sum(self.matrix[i][k] * other.matrix[k][j] for k in range(self.cols))
 
             return Matrix(data=res) if len(res) != 1 else Vector(data=res[0])
         elif isinstance(other, (int, float)):
@@ -70,13 +121,16 @@ class Matrix:
         elif isinstance(other, SLAU):
             return SLAU(self * other.matrix, other.x)
         else:
-            raise TypeError("Неподходящий множитель для матрицы")
+            raise TypeError(f"Неподходящий множитель для матрицы {self}:" + other)
 
-    def __str__(self):
+    def pretty_str(self):
         ans = ""
         for i in self.matrix:
             ans += '(' + ' '.join(map(str, i)) + ')' + '\n'
         return ans.strip()
+
+    def __str__(self):
+        return str(self.matrix)
 
     def __repr__(self):
         return str(self.matrix)
@@ -88,6 +142,19 @@ class Matrix:
 
     def determinant(self):
         return determinant(self.matrix)
+
+    def rang(self):
+        k = min(self.rows, self.cols)
+        while k > 1:
+            c1 = list(combinations(range(self.rows), k))
+            c2 = list(combinations(range(self.cols), k))
+            for o in range(len(c1)):
+                for p in range(len(c2)):
+                    mat = [[self.matrix[i][j] for j in c2[p]] for i in c1[o]]
+                    if determinant(mat) != 0:
+                        return k
+            k -= 1
+        return k
 
     def inverse(self):
         pass
@@ -106,7 +173,7 @@ class Vector(Matrix):
             super().__init__(data=[[]])
 
 
-class X(Vector):
+class X:
     def __init__(self):
         super().__init__()
 
@@ -114,7 +181,7 @@ class X(Vector):
         if isinstance(other, (Matrix, int, float)):
             return SLAU(other, self)
         else:
-            raise TypeError("Неподходящий множитель")
+            raise TypeError("Неподходящий множитель для X:" + other)
 
 
 class SLAU:
@@ -139,7 +206,7 @@ class SLAU:
             raise TypeError("Неподходящее слагаемое")
 
     def __str__(self):
-        return f"SLAU:Matrix={self.matrix.__repr__()}"
+        return f"SLAU:Matrix={self.matrix.__repr__()}+{self.free.__repr__()}"
 
     def __repr__(self):
         return self.__str__()
@@ -150,21 +217,43 @@ class SLAU:
         # print(res)
         return res
 
-    def solve(self):
-        if not isinstance(self.free, Vector):
-            raise ValueError("Неправильная формула")
-        if self.matrix.cols > self.matrix.rows:
-            return "Бесконечно решений"
-        if self.matrix.cols == self.matrix.rows:
-            det = self.matrix.determinant()
-            dets_i = [determinant(self.__cramer_mat_i(i)) for i in range(self.matrix.cols)]
+    def solve(self, gauss=True):
+        if not isinstance(self.free, Vector) and len(self.free.matrix[0]) != 1:
+            raise ValueError(f"Неправильная формула:\n\tМатрица = {self.matrix}\n\tСвободный член = {self.free}")
+        if len(self.free.matrix[0]) == 1:
+            self.free = Vector(data=[self.free.matrix[i][0] for i in range(self.free.rows)])
 
-            print(det)
-            print(dets_i)
+        RE, R = self.__rang(), self.matrix.rang()
+        print(RE, R)
 
-            return Vector(data=[i / det for i in dets_i])
-        else:
-            return SLAU(Matrix(data=self.matrix.matrix[:self.matrix.cols]), self.x, self.free).solve()
+        if RE > R:
+            return "Нет решений"
+        if RE == R:
+            if R != self.matrix.cols:
+                return "Бесконечное множество решений"
+
+            if gauss:
+                return self.gauss_solve()
+            else:
+                det = self.matrix.determinant()
+                if det == 0:
+                    return "абоб"
+                dets_i = [determinant(self.__cramer_mat_i(i)) for i in range(self.matrix.cols)]
+
+                print(det)
+                print(dets_i)
+
+                return [i / det for i in dets_i]
 
     def gauss_solve(self):
-        pass
+        a = gauss(self.matrix.matrix, self.free.matrix[0])
+        if a is not None:
+            return a
+        else:
+            return "Нет Решений"
+
+    def __rang(self):
+        mat = [i[:] for i in self.matrix.matrix]
+        for i in range(self.matrix.rows):
+            mat[i] += [self.free.matrix[0][i]]
+        return Matrix(data=mat).rang()
